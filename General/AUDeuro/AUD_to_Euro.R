eccbec8a2344a12bf69c9ae887651a70c3a0e80e
Stratos Pateloudis <stratospate@gmail.com>
2024-04-15 13:59:15 +0200

    AUDEUR predictions

--- General/AUDeuro/AUD_to_Euro.R
+++ General/AUDeuro/AUD_to_Euro.R
@@ -1,69 +1,78 @@
-library(fpp3)
+library("fpp3")
+library("patchwork")
+library("quantmod")
+library("PerformanceAnalytics")
 
-## DAta from : https://www.rba.gov.au/statistics/historical-data.html
+y_data <- getSymbols("AUDEUR=X", auto.assign = F)
 
-data <- read.csv('/Users/stratos/Documents/Rstudio_projects/AUDeuro/AUD_historical.csv')
-data$Date <- as.Date(data$Date, format = "%Y-%m-%d")
-data <- data %>% as_tsibble(index = Date, key = NULL)
-#update_tsibble(index = day, regular=TRUE )
+y_data <- Ad(y_data)%>%na.omit()
+y_data$Return <- dailyReturn(y_data$`AUDEUR=X.Adjusted`)
+colnames(y_data) <-c("Ad_Close", "Return")
+tail(y_data)
 
-loss =28000*(data$EUR) - (28000*0.714)
+xts_data <- y_data#%>%select(c("daily_return"))
 
-data %>% autoplot(loss)
- data %>% autoplot(EUR)
-data <- data %>% mutate(day=row_number())%>%
-  update_tsibble(index = day, regular=TRUE )
-tail(data)
+xts_data <- na.omit(xts_data)
 
+
+tail(xts_data)
+
+charts.PerformanceSummary(xts_data$Return)
+
+
+n_bins = 800
+hist(xts_data$Return,breaks = n_bins)
+
+mu <- mean(xts_data$Return)
+sigma <-sd(xts_data$Return)
+mu
+sigma
+
+target <- 0.01
+z <- (target - mu)/sigma
+z 
+
+pnorm(q=z, lower.tail = F)
 
 data %>% gg_tsdisplay(difference(log(EUR)), plot_type = 'partial')
 
-ar_fit <- data%>%
-  model(ar = ARIMA(EUR),
-        search=ARIMA(EUR, stepwise = FALSE, approx = FALSE), 
-        
-)
-ar_fit
 
-#ar_fit %>% pivot_longer(EUR, names_to="models", 
-                        #values_to = 'orders')
-        
-length(data$day)
+last_day <- length(data$day)
+last_day
+train_data <- data %>% filter(day <= last_day)
 
-train_data <- data %>% filter(day <= 3436)
 fit_1 <- train_data %>%
   model(#ANN = ETS(EUR ~ trend('A') ),
         ar = ARIMA(EUR, stepwise = FALSE, approx = FALSE),
-        search=ARIMA(EUR, stepwise = FALSE, approx = FALSE), 
+        #search=ARIMA(EUR, stepwise = FALSE, approx = FALSE), 
         )
 report(fit_1)
 
-#components(fit_1)%>%autoplot()
+fit_1 %>% select(ar)%>%gg_tsresiduals()
+augment(fit_1) %>%
+  filter(.model == "ar") %>%
+  features(.innov, ljung_box, lag=60, dof = 0) 
 
-#ar_fit %>% select(ar) %>% gg_tsresiduals()
-
-#fit_1 %>% select(ran) %>%gg_tsresiduals()
-
-#fit_1%>% forecast(h=50)%>%autoplot()
-#ar_fit%>% forecast(h=50)%>%autoplot()
-
+horizon = 5
 
 recent_data <- data %>% filter(day > 3400)
-pred_1 <- fit_1 %>% forecast(h=2, times = 50, bootstrap = TRUE) 
-pred_1 %>% autoplot(recent_data, level = NULL)
+pred_1 <- fit_1 %>% forecast(h=horizon, times = 100, bootstrap = TRUE) 
+pred_1 %>% autoplot(recent_data) + ggtitle(paste("Forecast for the next", horizon , "days"))
 print(pred_1)
+
+ggsave("predictions_ARIMA.pdf",
+  plot = last_plot())
 
 tail(recent_data)
 
-#report(ar_fit)
 
-sim <- fit_1%>% select(ar) %>% generate(h = 3, times = 5, bootstrap = TRUE)
+sim <- fit_1%>% select(ar) %>% generate(h = 15, times = 50, bootstrap = TRUE)
 recent_data %>%
   ggplot(aes(x = day)) +
   geom_line(aes(y = EUR)) +
   geom_line(aes(y = .sim, colour = as.factor(.rep)),
             data = sim) +
-  labs(title="AUD Exchange rate", y="Euro" ) +
+  labs(title="AUD Exchange rate simulations", y="Euro" ) +
   guides(colour = "none")
 
 
